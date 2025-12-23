c7dd2894652da45db778c8d8b88021a3894d4675
Deependra Kumar <deependrakumar07906@gmail.com>
2025-12-23 18:17:26 +0530

    Update media

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use("/api/media", mediaRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, 'localhost', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
